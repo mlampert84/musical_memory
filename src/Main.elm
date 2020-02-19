20434215cb61e05b2927782b0dfc0c0dc52b2b3a
Marcus Lampert <marcus.lampert@gmail.com>
2020-02-19 09:19:38 +0100

    Make cards in grid

--- src/Main.elm
+++ src/Main.elm
@@ -1,8 +1,72 @@
 module Main exposing (main)
 
-import Html exposing (text)
+import Browser exposing (sandbox)
+import Html exposing (Html, div, text)
+import Html.Attributes exposing (class, style)
+import List exposing (range)
 
 
-main : Html.Html a
+
+-- MAIN
+
+
 main =
-    text "testing again"
+    Browser.sandbox { init = init, update = update, view = view }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    { height : Int
+    , width : Int
+    }
+
+
+init : Model
+init =
+    { height = 6, width = 7 }
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Change String
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Change newContent ->
+            model
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    let
+        cssColumns =
+            "repeat( " ++ String.fromInt model.width ++ ", 1fr)"
+    in
+    div [ class "grid-container", style "grid-template-columns" cssColumns ]
+        (cells model.width model.height)
+
+
+cells : Int -> Int -> List (Html Msg)
+cells w h =
+    List.concat <| List.map (makeRow w) <| List.range 1 h
+
+
+makeRow : Int -> Int -> List (Html Msg)
+makeRow width height =
+    let
+        toDiv x =
+            div [ class "grid-item" ] [ text (String.fromInt (x + width * (height - 1))) ]
+    in
+    List.map toDiv <| List.range 1 width
